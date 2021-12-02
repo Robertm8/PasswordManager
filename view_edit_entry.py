8b06481b0519c885bcc02d5ac557e936f5872aba
Robertm8 <71862295+Robertm8@users.noreply.github.com>
2021-12-01 16:14:00 -0800

    View / Edit screen refactored to remove nested functions and shorten function length.

--- view_edit_entry.py
+++ view_edit_entry.py
@@ -1,5 +1,17 @@
 from settings import *
 
+
+def fetch_data(website):
+    """
+    Runs query to database and unencrypts the data.
+    :param website: website that was selected
+    :return: decrypted results
+    """
+    c.execute("SELECT url, username, password, uid FROM data where url = '" + website + "'")
+    results = c.fetchall()
+    return translate_results(results)
+
+
 def translate_results(results):
     """
     Function for decrypting usernames / passwords for a website.
@@ -35,92 +47,189 @@ def translate_results(results):
 
     return decrypted_results
 
+
+def update_password_field(clicked, decrypted_results, password_entry):
+    """
+    Updates the password field upon selecting a username. Returns first match it finds.
+    :param clicked: tkinter string variable
+    :param decrypted_results: list of usernames and passwords
+    :param password_entry: the entry element where password is located
+    :return: None
+    """
+    click = clicked.get()
+    for element in decrypted_results:
+        if element[1] == click:
+            password_entry.delete(0, END)
+            password_entry.insert(0, element[2])
+            break
+
+
+def copy_username(window, clicked):
+    """
+    Copies username to the clipboard.
+    :param window: window where button that calls this function resides
+    :param clicked: tkinter string variable
+    :return: None
+    """
+    selected = clicked.get()
+    window.clipboard_clear()
+    window.clipboard_append(selected)
+
+
+def copy_password(window, password_entry):
+    """
+    Copies password to the clipboard.
+    :param window: window where button that calls this function resides
+    :param password_entry: password text field element
+    :return: None
+    """
+    selected = password_entry.get()
+    window.clipboard_clear()
+    window.clipboard_append(selected)
+
+
+def delete_click(window, decrypted_results, clicked):
+    """
+    Deletes the username/password combo from the database.
+    :param window: window where button that calls this function resides
+    :param decrypted_results: list of usernames and passwords
+    :param clicked: tkinter string variable
+    :return:
+    """
+    answer = messagebox.askyesno('Delete', 'Are you sure you want to delete?', parent=window)
+    if not answer:
+        return
+    selected = clicked.get()
+    entry = None
+    for element in decrypted_results:
+        if element[1] == selected:
+            entry = element
+            break
+    c.execute("DELETE FROM data WHERE uid = '" + entry[3] + "';")
+    conn.commit()
+    window.destroy()
+
+
 def initialize(window, website):
     """
-    Sets up main window elements.
+    Manager function that calls helper functions to define elements and then draw elements.
     :param window: the window popup
     :return: None
     """
-    # Fetch database entries
-    c.execute("SELECT url, username, password, uid FROM data where url = '" + website + "'")
-    results = c.fetchall()
-    decrypted_results = translate_results(results)
+    elements = element_definitions(window, website)
+    draw_elements(elements)
+    create_tooltips(elements)
 
-    # Nested functions
-    def delete_click():
-        answer = messagebox.askyesno('Delete', 'Are you sure you want to delete?', parent=window)
-        if not answer:
-            return
-        selected = clicked.get()
-        entry = None
-        for element in decrypted_results:
-            if element[1] == selected:
-                entry = element
-                break
-        c.execute("DELETE FROM data WHERE uid = '" + entry[3] + "';")
-        conn.commit()
-        window.destroy()
 
-    def copy_username():
-        """
-        Copies username to the clipboard.
-        :return: None
-        """
-        selected = clicked.get()
-        window.clipboard_clear()
-        window.clipboard_append(selected)
+def element_definitions(window, website):
+    """
+    Manager for element definitions.
+    :param window: the screen where the elements will exist
+    :return: tuple of element groups
+    """
+    clicked = StringVar()
+    decrypted_results = fetch_data(website)  # db query
 
-    def copy_password():
-        """
-        Copies password to the clipboard.
-        :return: None
-        """
-        selected = password_entry.get()
-        window.clipboard_clear()
-        window.clipboard_append(selected)
+    labels = label_definitions(window, website)
+    entry = entry_definition(window)
+    dropdown = dropdown_definition(window, entry, clicked, decrypted_results)
+    buttons = button_definitions(window, clicked, entry, decrypted_results)
 
-    def update_password_field(click):
-        """
-        Updates the password field upon selecting a username. Returns first match it finds.
-        :param click: string of what was clicked in the drop down menu
-        :return: None
-        """
-        for element in decrypted_results:
-            if element[1] == click:
-                password_entry.delete(0, END)
-                password_entry.insert(0, element[2])
-                break
+    return labels, dropdown, entry, buttons
 
-    # Label definitions
+
+def label_definitions(window, website):
+    """
+    Creates the text on the screen.
+    :param window: the screen where the text will exist
+    :param website: the website that was selected
+    :return: tuple of text labels
+    """
     username = Label(window, text='Username:')
     password = Label(window, text='Password:')
     website_label = Label(window, text=website)
 
-    # Dropdown definitions
-    clicked = StringVar()
+    return username, password, website_label
+
+
+def dropdown_definition(window, entry, clicked, decrypted_results):
+    """
+    Creates dropdown menu populated with usernames.
+    :param window: the screen where the dropdown exists
+    :param entry: the password field
+    :param clicked: tkinter string variable
+    :param decrypted_results: unencrypted data from database
+    :return:
+    """
     usernames = [user[1] for user in decrypted_results]
-    username_dropdown = OptionMenu(window, clicked, *usernames, command=update_password_field)
+    clicked.set(usernames[0])  # initial selection
+    update_password_field(clicked, decrypted_results, entry)  # password call for the initial selection
 
-    # Input definitions
-    password_entry = Entry(window)
+    username_dropdown = OptionMenu(window, clicked, *usernames,
+                                   command=lambda _: update_password_field(clicked, decrypted_results, entry))
 
-    # Button definition
-    copy_username_button = Button(window, text='Copy', command=copy_username)
-    copy_password_button = Button(window, text='Copy', command=copy_password)
-    # update_button = Button(window, text='Update', command=update_password)  # currently unavailable
-    delete_button = Button(window, text='Delete', command=delete_click)
+    return username_dropdown
 
-    # Draw elements to window
+
+def entry_definition(window):
+    """
+    Entry field where password will be displayed.
+    :param window: the screen where entry box is located
+    :return: entry element
+    """
+    entry = Entry(window)
+    return entry
+
+
+def button_definitions(window, clicked, entry, decrypted_results):
+    """
+    Definition of various buttons located on the screen. Buttons are both copy buttons and delete button.
+    :param window: screen where buttons are located
+    :param clicked: tkinter string variable
+    :param entry: password field on screen
+    :param decrypted_results: unencrypted data from database
+    :return: tuple of button elements
+    """
+    copy_username_button = Button(window, text='Copy', command=lambda: copy_username(window, clicked))
+    copy_password_button = Button(window, text='Copy', command=lambda: copy_password(window, entry))
+    delete_button = Button(window, text='Delete', command=lambda: delete_click(window, decrypted_results, clicked))
+
+    return copy_username_button, copy_password_button, delete_button
+
+
+def draw_elements(elements):
+    """
+    Takes a tuple of element groups and draws them to the screen.
+    :param elements: tuple containing group of elements. The group of elements may also be a tuple.
+    :return: None
+    """
+    # Unpack elements
+    labels, dropdown, entry, buttons = elements
+    username, password, website = labels
+    copy_username_button, copy_password_button, delete_button = buttons
+
+    # Draw elements
     username.grid(row=0, column=0)
-    username_dropdown.grid(row=1, column=0)
+    dropdown.grid(row=1, column=0)
     copy_username_button.grid(row=1, column=1)
     password.grid(row=2, column=0)
-    password_entry.grid(row=3, column=0, padx=(20, 10))
+    entry.grid(row=3, column=0, padx=(20, 10))
     copy_password_button.grid(row=3, column=1, padx=10)
     delete_button.grid(row=4, column=1)
-    website_label.grid(row=5, column=0, columnspan=2, sticky=S)
+    website.grid(row=5, column=0, columnspan=2, sticky=S)
 
-    # Tooltips
-    ToolTip(copy_username_button, msg='Copies the username.', delay=0.5)
-    ToolTip(copy_password_button, msg='Copies the password.', delay=0.5)
-    ToolTip(delete_button, msg='Deletes the entry from the database.', delay=0.5)
+
+def create_tooltips(elements):
+    """
+    Creates tooltips for the different buttons on the screen.
+    :param elements: package of elements that buttons are stored in
+    :return: None
+    """
+    # Unpack buttons
+    buttons = elements[3]
+    copy_username, copy_password, delete = buttons
+
+    # Create tooltips
+    ToolTip(copy_username, msg='Copies the username.', delay=0.5)
+    ToolTip(copy_password, msg='Copies the password.', delay=0.5)
+    ToolTip(delete, msg='Deletes the entry from the database.', delay=0.5)
